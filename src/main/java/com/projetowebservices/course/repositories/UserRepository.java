66192639c2c9fadb968beda03c0524db1010c5a6
Andre Fukuda <andre.alex.fukuda@gmail.com>
2024-08-13 13:30:16 -0300

    Service layer, component registration

--- src/main/java/com/projetowebservices/course/repositories/UserRepository.java
+++ src/main/java/com/projetowebservices/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projetowebservices.course.entities.User;
 
+//não é necessário, pois já está herdando do JpaRepository
 public interface UserRepository extends JpaRepository<User, Long>{//já está usando a entidade (user) e seu tipo (long), então esta pronto
 
 }
